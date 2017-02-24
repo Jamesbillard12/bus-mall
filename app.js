ff61bda90f7d1df32b8671d364d6e07057dd6832
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-02-23 21:58:21 -0800

    got three random images to populate

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1. Generate three random, non-dupe images (part of the controller)
 // 2. Object constructor for Products:
-  // a. Include name, path, votes
+// a. Include name, path, votes
 // 3. A tracker object that will controll functionality of app
 // 4. Event listener(s) for image clicks
 
@@ -21,7 +21,30 @@ console.log(productsArray);
 
 // a simple IIFE to build all the product images
 (function() {
-  // build the objects
+  var num0 = Math.floor(Math.random() * productsArray.length);
+  console.log(num0);
+  var getDivZero = document.getElementById('zero')
+  var img0 = document.createElement('img');
+  img0.src = productsArray[num0].path;
+  console.log(productsArray[num0].path);
+  zero.appendChild(img0);
+
+  var num1 = Math.floor(Math.random() * productsArray.length);
+  console.log(num1);
+  var getDivOne = document.getElementById('one')
+  var img1 = document.createElement('img');
+  img1.src = productsArray[num1].path;
+  console.log(productsArray[num1].path);
+  zero.appendChild(img1);
+
+  var num2 = Math.floor(Math.random() * productsArray.length);
+  console.log(num2);
+  var getDivOne = document.getElementById('two')
+  var img2 = document.createElement('img');
+  img2.src = productsArray[num2].path;
+  console.log(productsArray[num2].path);
+  zero.appendChild(img2);
+
 })()
 
 var tracker = {
