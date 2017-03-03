9aa911fdc15e108460aaa1e9f8dc27578ddd64b1
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-02 21:40:08 -0800

    refactured long blocks into short methods

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ function Product(name, path, color) {
   this.path = path;
   this.votes = 0;
   this.colors = color;
-  console.log(this.colors);
   productsArray.push(this);
   chartData.data.datasets[0].data.push(this.votes);
   chartData.data.datasets[0].backgroundColor.push(this.colors)
@@ -57,14 +56,13 @@ function Product(name, path, color) {
 
 //where all the functionality is controlled from
 var tracker = {
-// this generates a random number between 0-18
+  // this generates a random number between 0-18
   randNumberGenerator: function(){
     return Math.round(Math.random() * (productsArray.length - 1));
   },
-// this function pushes 3 randomly generated numbers into an array called randomNums
+  // this function pushes 3 randomly generated numbers into an array called randomNums
   chooseThreeRandomPics: function(){
     var randomNums = [];
-
     while (randomNums.length < 3) {
       var possible = tracker.randNumberGenerator();
       if (randomNums.indexOf(possible) === -1) {
@@ -73,9 +71,8 @@ var tracker = {
     }
     return randomNums;
   },
-// this is where the the images are rendered into img selection area of the page
+  // this is where the the images are rendered into img selection area of the page
   renderImgsToDom: function(){
-    console.log('tracker.renderImgsToDom');
     var threeRandomIndices = tracker.chooseThreeRandomPics();
     var pictureHolder = document.getElementById('pictureHolder');
     for (var i = 0; i < threeRandomIndices.length; i++) {
@@ -86,39 +83,37 @@ var tracker = {
       pictureHolder.appendChild(img);
     }
   },
-// this is where the vote and counter are added on click event
+  // this is where the vote and counter are added on click event
   tallyVoteCounter: function(event){
-
-
     counter += 1;
+    tracker.setPics();
     console.log('counter', counter);
-    // for (var i = 0; i < productsArray.length; i++)
     for (var i in productsArray) {
       if (productsArray[i].name === event.target.id) {
         productsArray[i].votes++;
-        myChart.data.datasets[0].data[i]++;
+        // myChart.data.datasets[0].data[i]++;
         myChart.update();
-
-// this deletes the pictures once one is clicked on
-        var images = document.getElementsByTagName('img');
-        while(images.length > 0) {
-          images[0].parentNode.removeChild(images[0]);
-        }
-
-// this gets a new set of pictures until 15 votes have been made. At this point, the eventlistener is shut off.
-        tracker.renderImgsToDom();
-        console.log('votes', productsArray[i].votes);
-        console.log('chart', myChart.data.datasets[0].data[i]++);
-        if (counter === 15) {
-          document.getElementById("pictureHolder").removeEventListener("click",tracker.tallyVoteCounter);
-          document.getElementById('results').addEventListener('click', tracker.renderResults);
-        }
       }
     }
-    console.log('event: ', event.target.id);
-
   },
-//this will render the results when the html button is clicked.
+  deletePics: function(){
+    // this deletes the pictures once one is clicked on
+    var images = document.getElementsByTagName('img');
+    while(images.length > 0) {
+      images[0].parentNode.removeChild(images[0]);
+    }
+  },
+
+  setPics: function(){
+    // this gets a new set of pictures until 15 votes have been made. At this point, the eventlistener is shut off.
+    tracker.deletePics();
+    tracker.renderImgsToDom();
+    if (counter === 15) {
+      console.log('counter', counter);
+      document.getElementById("pictureHolder").removeEventListener("click",tracker.tallyVoteCounter);
+      document.getElementById('results').addEventListener('click', tracker.renderResults);
+    }
+  },
   renderResults: function() {
     var getUl = document.getElementById('rendered-results');
     for (var i in productsArray) {
@@ -126,28 +121,17 @@ var tracker = {
       li.textContent = 'The ' + productsArray[i].name + ': ' + productsArray[i].votes + ' votes.';
       getUl.appendChild(li);
     }
-  }
-}
+  },
+};
+
+
+
+//this will render the results when the html button is clicked.
+
 
 var ctx = document.getElementById("myChart").getContext("2d");
 
 var myChart = new Chart(ctx, chartData);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 document.getElementById('pictureHolder').addEventListener('click', tracker.tallyVoteCounter);
 tracker.renderImgsToDom();
