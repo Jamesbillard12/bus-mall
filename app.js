3bc2ffa4a03c42d1836522e4488d32fe5ae1c45e
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-02 22:20:15 -0800

    Chart really update this time and the votes checkout

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ function Product(name, path, color) {
   this.votes = 0;
   this.colors = color;
   productsArray.push(this);
+  chartData.data.datasets[0].data.push(this.votes);
   chartData.data.datasets[0].backgroundColor.push(this.colors);
 }
 // a simple IIFE to build all the product objects
@@ -90,9 +91,9 @@ var tracker = {
     for (var i in productsArray) {
       if (productsArray[i].name === event.target.id) {
         productsArray[i].votes++;
-        chartData.data.datasets[0].data.push(productsArray[i].votes);
-        console.log(productsArray[i].name, productsArray[i].votes);
+        myChart.data.datasets[0].data[i]++;
         myChart.update();
+        console.log(productsArray[i].name, productsArray[i].votes);
       }
     }
   },
